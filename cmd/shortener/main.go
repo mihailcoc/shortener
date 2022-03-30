8a6f91b899eb92f18642a51f17e704171ecb78f1
charlize <charlize@debian-BULLSEYE-live-builder-AMD64>
2022-03-30 13:24:58 +0300

    My first commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,11 +4,15 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"math/rand"
 	"net/http"
+	"net/url"
 	"strconv"
 )
 
 func viewHandler(w http.ResponseWriter, r *http.Request) {
+	//var m map[string]string
+	m := make(map[string]string)
 
 	switch r.Method {
 	// если методом POST
@@ -21,11 +25,26 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 			http.Error(w, err.Error(), 500)
 			return
 		}
-		//length := len(b) / 2
-		//UnicStr = b[:length]
-		responseURL := "https://" + r.Host + r.URL.String() + string(b)
+		//fmt.Println(string(b))
+		//fmt.Println("r.Body", string(b))
+		u, err := url.Parse(string(b))
+		if err != nil {
+			panic(err)
+		}
+		//fmt.Println(u)
+		//fmt.Println("Path:", u.Path)
+		//fmt.Println("len(Path):", len(u.Path))
+		//fmt.Println("randomString", randomString(len(u.Path)/4))
+		//Генерируем ключ
+		m_key := randomString(len(u.Path) / 4)
+		//По ключу помещаем значение localhost map.
+		m[m_key] = u.Path
+		//Генерируем ответ
+		responseURL := "http://" + r.Host + r.URL.String() + m_key
+
+		fmt.Println(responseURL)
 		w.Write([]byte(responseURL))
-		fmt.Fprint(w)
+		//fmt.Fprint(w)
 	// если методом GET
 	case "GET":
 		id, err := strconv.Atoi(r.URL.Query().Get("id"))
@@ -34,9 +53,12 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 		// устанавливаем в заголовке оригинальный URL
-		w.Header().Set("Location", "id")
+		fmt.Println("id", id)
+		origURL := m[string(id)]
+		w.Header().Set("Location", origURL)
 		// устанавливаем статус-код 307
 		w.WriteHeader(http.StatusTemporaryRedirect)
+		// отдаем редирект на собственный url и код 307
 		fmt.Fprint(w)
 	default:
 		http.Error(w, "Only GET and POST requests are allowed!", http.StatusMethodNotAllowed)
@@ -44,8 +66,20 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	}
 }
 
+func randomInt(min, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func randomString(len int) string {
+	bytes := make([]byte, len)
+	for i := 0; i < len; i++ {
+		bytes[i] = byte(randomInt(97, 122))
+	}
+	return string(bytes)
+}
+
 func main() {
 	http.HandleFunc("/", viewHandler)
-	err := http.ListenAndServe("localhost:8080", nil)
+	err := http.ListenAndServe("localhost:8000", nil)
 	log.Fatal(err)
 }
