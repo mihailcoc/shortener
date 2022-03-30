4764d6227a59e59e582deea784f993dfa37786dc
charlize <charlize@debian-BULLSEYE-live-builder-AMD64>
2022-03-30 15:06:04 +0300

    My first commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -46,7 +46,7 @@ func viewHandler(w http.ResponseWriter, r *http.Request) {
 	// если методом GET
 	case "GET":
 		// извлекаем фрагмент id из URL запроса GET /{id}
-		q := r.URL.Query().Get("id")
+		q := r.URL.Path
 		if q == "" {
 			http.Error(w, "The query parameter is missing", http.StatusBadRequest)
 			return
