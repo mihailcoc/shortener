fd2cfa2b9e0cab58153c5ca93bdab785d8db9eca
charlize <charlize@debian-BULLSEYE-live-builder-AMD64>
2022-03-31 11:25:33 +0300

    My first commit

--- internal/app/main.go
+++ internal/app/main.go
@@ -1,64 +1,24 @@
 package main
 
 import (
-	"bufio"
-	"bytes"
-	"fmt"
-	"io"
 	"net/http"
-	"net/url"
-	"os"
-	"strconv"
-	"strings"
+	"testing"
 )
 
-func main() {
-	// адрес сервиса (как его писать, расскажем в следующем уроке)
-	endpoint := "http://localhost:8080/"
-	// контейнер данных для запроса
-	data := url.Values{}
-	// приглашение в консоли
-	fmt.Println("Введите длинный URL")
-	// открываем потоковое чтение из консоли
-	reader := bufio.NewReader(os.Stdin)
-	// читаем строку из консоли
-	long, err := reader.ReadString('\n')
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+func Test_viewHandler(t *testing.T) {
+	type args struct {
+		w http.ResponseWriter
+		r *http.Request
 	}
-	long = strings.TrimSuffix(long, "\n")
-	// заполняем контейнер данными
-	data.Set("url", long)
-	// конструируем HTTP-клиент
-	client := &http.Client{}
-	// конструируем запрос
-	// запрос методом POST должен, кроме заголовков, содержать тело
-	// тело должно быть источником потокового чтения io.Reader
-	// в большинстве случаев отлично подходит bytes.Buffer
-	request, err := http.NewRequest(http.MethodPost, endpoint, bytes.NewBufferString(data.Encode()))
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
 	}
-	// в заголовках запроса сообщаем, что данные кодированы стандартной URL-схемой
-	request.Header.Add("Content-Type", "application/x-www-form-urlencoded")
-	request.Header.Add("Content-Length", strconv.Itoa(len(data.Encode())))
-	// отправляем запрос и получаем ответ
-	response, err := client.Do(request)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			viewHandler(tt.args.w, tt.args.r)
+		})
 	}
-	// печатаем код ответа
-	fmt.Println("Статус-код ", response.Status)
-	defer response.Body.Close()
-	// читаем поток из тела ответа
-	body, err := io.ReadAll(response.Body)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-	// и печатаем его
-	fmt.Println(string(body))
 }
