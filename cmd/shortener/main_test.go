57e6306d0d89756bd94b1f4c1512eb5332106b05
mihailcoc <mihailcoc@yandex.ru>
2022-04-03 13:48:26 +0300

    My third commit

--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,67 +1,41 @@
-package shortener
+package main
 
 import (
-	"io"
-	"net/http"
-	"net/http/httptest"
 	"testing"
-	"yandex-must-have/handlers"
+
+	"github.com/gin-gonic/gin"
 )
 
-func TestStatusHandler(t *testing.T) {
-	// определяем структуру теста
-	type want struct {
-		code        int
-		response    string
-		contentType string
+func Test_handlerGet(t *testing.T) {
+	type args struct {
+		g *gin.Context
 	}
-	// создаём массив тестов: имя и желаемый результат
 	tests := []struct {
 		name string
-		want want
+		args args
 	}{
-		// определяем все тесты
-		{
-			name: "positive test #1",
-			want: want{
-				code:        200,
-				response:    `{"status":"ok"}`,
-				contentType: "application/json",
-			},
-		},
+		// TODO: Add test cases.
 	}
 	for _, tt := range tests {
-		// запускаем каждый тест
 		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(http.MethodGet, "/status", nil)
+			handlerGet(tt.args.g)
+		})
+	}
+}
 
-			// создаём новый Recorder
-			w := httptest.NewRecorder()
-			// определяем хендлер
-			h := http.HandlerFunc(handlers.StatusHandler)
-			// запускаем сервер
-			h.ServeHTTP(w, request)
-			res := w.Result()
-
-			// проверяем код ответа
-			if res.StatusCode != tt.want.code {
-				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
-			}
-
-			// получаем и проверяем тело запроса
-			defer res.Body.Close()
-			resBody, err := io.ReadAll(res.Body)
-			if err != nil {
-				t.Fatal(err)
-			}
-			if string(resBody) != tt.want.response {
-				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
-			}
-
-			// заголовок ответа
-			if res.Header.Get("Content-Type") != tt.want.contentType {
-				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
-			}
+func Test_handlerPost(t *testing.T) {
+	type args struct {
+		g *gin.Context
+	}
+	tests := []struct {
+		name string
+		args args
+	}{
+		// TODO: Add test cases.
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			handlerPost(tt.args.g)
 		})
 	}
 }
