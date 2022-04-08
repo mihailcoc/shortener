10427f3b2e3d68b7fe47d435f6d2f5e85a1b594e
mihailcoc <mihailcoc@yandex.ru>
2022-04-08 11:28:46 +0300

    My third commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,11 +1,6 @@
 package main
 
 import (
-	"fmt"
-	"io"
-	"math/rand"
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -22,41 +17,4 @@ func main() {
 	server.GET("/:key", handlerGet)
 	server.POST("/", handlerPost)
 	server.Run(addr)
-}
-
-func handlerGet(g *gin.Context) {
-	key := g.Param("key")
-	if url, ok := urls[key]; ok {
-		g.Redirect(http.StatusTemporaryRedirect, url)
-		return
-	} else {
-		g.String(http.StatusNotFound, "url not found")
-		return
-	}
-}
-
-func handlerPost(g *gin.Context) {
-	body, err := io.ReadAll(g.Request.Body)
-	if err != nil {
-		g.String(http.StatusBadRequest, "bad request")
-		return
-	}
-	mKey := randomString(len(body) / 4)
-
-	urls[mKey] = string(body)
-
-	response := fmt.Sprintf("%s/%s", baseURL, mKey)
-	g.String(http.StatusCreated, response)
-}
-
-func randomInt(min, max int) int {
-	return min + rand.Intn(max-min)
-}
-
-func randomString(len int) string {
-	bytes := make([]byte, len)
-	for i := 0; i < len; i++ {
-		bytes[i] = byte(randomInt(97, 122))
-	}
-	return string(bytes)
 }
--- cmd/shortener/main_test.go
+++ cmd/shortener/main_test.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"testing"
-
 	"github.com/gin-gonic/gin"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+	"io"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
 )
 
 func Test_handlerGet(t *testing.T) {
@@ -15,10 +20,47 @@ func Test_handlerGet(t *testing.T) {
 		args args
 	}{
 		// TODO: Add test cases.
+		{
+			name: "positive test #1",
+			want: want{
+				code:        307,
+				response:    `{"status":"redirect"}`,
+				contentType: "application/json",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			handlerGet(tt.args.g)
+			request := httptest.NewRequest(http.MethodGet, "/status", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(handlerGet)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+			// заголовок ответа
+			if res.Header.Get("Content-Type") != tt.want.contentType {
+				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
+			}
 		})
 	}
 }
@@ -31,11 +73,87 @@ func Test_handlerPost(t *testing.T) {
 		name string
 		args args
 	}{
-		// TODO: Add test cases.
+		{
+			// TODO: Add test cases.
+			name: "positive test #2",
+			want: want{
+				code:        200,
+				response:    `{"status":"redirect"}`,
+				contentType: "application/json",
+			},
+		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			handlerPost(tt.args.g)
+			request := httptest.NewRequest(http.MethodPost, "/status", nil)
+
+			// создаём новый Recorder
+			w := httptest.NewRecorder()
+			// определяем хендлер
+			h := http.HandlerFunc(handlerPost)
+			// запускаем сервер
+			h.ServeHTTP(w, request)
+			res := w.Result()
+
+			// проверяем код ответа
+			if res.StatusCode != tt.want.code {
+				t.Errorf("Expected status code %d, got %d", tt.want.code, w.Code)
+			}
+
+			// получаем и проверяем тело запроса
+			defer res.Body.Close()
+			resBody, err := io.ReadAll(res.Body)
+			if err != nil {
+				t.Fatal(err)
+			}
+			if string(resBody) != tt.want.response {
+				t.Errorf("Expected body %s, got %s", tt.want.response, w.Body.String())
+			}
+
+			// заголовок ответа
+			if res.Header.Get("Content-Type") != tt.want.contentType {
+				t.Errorf("Expected Content-Type %s, got %s", tt.want.contentType, res.Header.Get("Content-Type"))
+			}
 		})
 	}
+}
+
+func testRequest(t *testing.T, ts *httptest.Server, method, path string) (*http.Response, string) {
+	req, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := http.DefaultClient.Do(req)
+	require.NoError(t, err)
+
+	respBody, err := ioutil.ReadAll(resp.Body)
+	require.NoError(t, err)
+
+	defer resp.Body.Close()
+
+	return resp, string(respBody)
+}
+
+func TestRouter(t *testing.T) {
+	r := NewRouter()
+	ts := httptest.NewServer(r)
+	defer ts.Close()
+
+	resp, body := testRequest(t, ts, "GET", "/")
+	assert.Equal(t, http.StatusBadRequest, resp.StatusCode)
+	assert.Equal(t, "", body)
+
+	b, err := io.ReadAll(r.Body)
+	// обрабатываем ошибку
+	if err != nil {
+		http.Error(w, err.Error(), 500)
+		return
+	}
+	mKey := randomString(len(b) / 4)
+	responseURL := "http://" + r.Host + r.URL.String() + mKey
+
+	resp, body = testRequest(t, ts, "POST", "/")
+	assert.Equal(t, http.StatusOK, resp.StatusCode)
+	assert.Equal(t, responseURL, body)
+
 }
