72d0738c8890ffd56edec36cc712e11d989c7f0c
mihailcoc <78297274+mihailcoc@users.noreply.github.com>
2022-04-14 11:50:59 +0300

    Revert "Inc3"

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,21 +1,3 @@
 package main
 
-import (
-	"github.com/gin-gonic/gin"
-)
-
-var (
-	addr    = "localhost:8080"
-	scheme  = "http"
-	baseURL = scheme + "://" + addr
-)
-
-func main() {
-	server := gin.Default()
-	server.GET(
-		"/:key",
-		handlerGet,
-	)
-	server.POST("/", handlerPost)
-	server.Run(addr)
-}
+func main() {}
