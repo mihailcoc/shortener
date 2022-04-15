40f11129f71543c94b39cb65f33f38ce9f59b8d2
mihailcoc <mihailcoc@yandex.ru>
2022-04-15 15:35:06 +0300

    My first commit

--- cmd/shortener/storage.go
+++ cmd/shortener/storage.go
@@ -1,14 +1,1 @@
 package main
-
-//var (
-//	urls = make(map[string]string)
-//)
-
-//type body = string
-
-//func storage (b *body) {
-//	var mKey = randomString(len(body()) / 4)
-//	return mKey
-//}
-
-//urls[mKey] = string(body)
