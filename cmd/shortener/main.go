d1d7c7d0e2c411bd732b932d12df1885bca08250
mihailcoc <78297274+mihailcoc@users.noreply.github.com>
2022-04-15 15:52:43 +0300

    Update main.go

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,1 +1,31 @@
+package main
 
+import (
+	"github.com/gin-gonic/gin"
+)
+
+type Config struct {
+	ServerAddress string `env:"localhost:8080"`
+	BaseURL       string `env:"localhost:8080"`
+}
+
+var (
+	addr    = "localhost:8080"
+	scheme  = "http"
+	baseURL = scheme + "://" + addr
+)
+
+func main() {
+	server := gin.Default()
+	server.GET(
+		"/:key",
+		handlerGet,
+	)
+	server.GET(
+		"/api/shorten:key",
+		handlerGetAPI,
+	)
+	server.POST("/", handlerPost)
+	server.POST("/api/shorten", handlerPostAPI)
+	server.Run(addr)
+}
