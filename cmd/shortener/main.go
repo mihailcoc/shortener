bad79a892d6f841be4a3697d0e8d145d9dae0443
charlize theron <mihailcoc@yandex.ru>
2022-04-20 17:10:04 +0300

    My second commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"log"
 	"net/http"
 
 	"github.com/gorilla/mux"
 )
 
-type Config struct {
-	Server_Address string `env:"localhost:8080"`
-	Base_Url       string `env:"localhost:8080"`
-}
+//type Config struct {
+//	Server_Address string `env:"localhost"`
+//	Base_Url       string `env:"http://localhost:8080"`
+//}
 
 var (
 	addr    = "localhost:8080"
@@ -18,22 +17,24 @@ var (
 	baseURL = scheme + "://" + addr
 )
 
-const port = ":8080"
+//const port = ":8080"
 
 func main() {
 	// init router
 	router := mux.NewRouter()
+	//router := http.NewRouter()
 
 	//os.Setenv("ServerAddress", "localhost"+port)
 	//os.Setenv("baseURL", "http:/"+os.Getenv("ServerAddress")+"/")
-	srv := http.Server{
-		//Addr:    addr,
-		Handler: router,
-	}
+	//srv := http.Server{
+	//	//Addr:    addr,
+	//	Handler: router,
+	//}
 
 	// router handler / endpoints
+	//router.HandleFunc("/", handlerPost).Methods("POST")
 	router.HandleFunc("/", handlerPost).Methods("POST")
 	router.HandleFunc("/:key", handlerGet).Methods("GET")
 	router.HandleFunc("/api/shorten", handlerPostAPI).Methods("POST")
-	log.Fatal(srv.ListenAndServe())
+	http.ListenAndServe(":8080", nil)
 }
