c84e1174ee8dc8ecf2e15064b1392d621e08cfb9
charlize theron <mihailcoc@yandex.ru>
2022-04-22 16:52:49 +0300

    My second commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -4,7 +4,6 @@ import (
 	"flag"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/caarlos0/env/v6"
 	"github.com/gorilla/mux"
@@ -13,17 +12,18 @@ import (
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:":8080"`
 	BaseURL       string `env:"localhost:8080"`
-	Host          string `env:"localhost:8080"`
 }
 
 var (
-	port          = ":8080"
-	ServerAddress = "localhost" + port
+	//port          = ":8080"
+	ServerAddress = "localhost" + ":8080"
 	scheme        = "http"
 	BaseURL       = scheme + "://" + ServerAddress
 )
 
 func main() {
+	log.Printf("ServerAddress")
+	log.Println(ServerAddress)
 	var cfg Config
 	err := env.Parse(&cfg)
 	if err != nil {
@@ -31,44 +31,27 @@ func main() {
 	}
 	log.Printf("cfg.ServerAddress: %s", cfg.ServerAddress)
 	//os.Setenv("port", ":8080")
-	os.Setenv("SERVER_ADDRESS", ":8080")
+	//os.Setenv("SERVER_ADDRESS", ":8080")
 	//os.Setenv("ServerAddress", "localhost"+os.Getenv("port"))
-	os.Setenv("BaseURL", "http:/localhost"+os.Getenv("ServerAddress")+"/")
+	//os.Setenv("BaseURL", "http:/localhost"+os.Getenv("ServerAddress")+"/")
 
 	ServerAddress := flag.String("a", "127.0.0.1:8000", "SERVER_ADDRESS - адрес для запуска HTTP-сервера")
 	flag.Parse()
 	//pflag.StringVarP(*ServerAddress, port)
 	log.Printf("1 &ServerAddress:")
 	log.Println(ServerAddress)
-	//flag.StringVar(&config.ServerAddress, "a", "127.0.0.1:8080", "server address")
-	//flag.Parse()
-	//log.Printf("ServerAddress: %s", ServerAddress)
 
-	//ServerAddress, exist := os.LookupEnv("ADDRESS")
-	//if exist {
-	//	config.ServerAddress = ServerAddress
-	//}
-	//log.Printf("ServerAddress: %s", ServerAddress)
-
-	if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
-		*ServerAddress = u
-	}
-	flag.Parse()
-	log.Printf("2 &SERVER_ADDRESS:")
-	log.Println(ServerAddress)
-
-	//port := flag.String("b", ":8000", "PORT - порт для запуска HTTP-сервера")
-	//if uport, f := os.LookupEnv("port"); f {
-	///	*port = uport
+	//if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
+	//	*ServerAddress = u
 	//}
 	//flag.Parse()
-	//log.Printf("port:")
-	//log.Println(port)
-	// init router
+	log.Printf("*ServerAddress перед сервером")
+	log.Println(*ServerAddress)
+
 	router := mux.NewRouter()
 
 	srv := http.Server{
-		Addr:    cfg.ServerAddress,
+		Addr:    *ServerAddress,
 		Handler: router,
 	}
 
