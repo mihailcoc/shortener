6bc692fa94113a6d72fa4c34fdfb4e41ce66985a
charlize theron <mihailcoc@yandex.ru>
2022-04-22 17:54:26 +0300

    My second commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -28,6 +28,7 @@ func main() {
 	log.Println(ServerAddress)
 	log.Printf("ServerAddress")
 	log.Println(ServerAddress)
+	// 1 вариант
 	//var cfg Config
 	//err := env.Parse(&cfg)
 	//if err != nil {
@@ -35,17 +36,20 @@ func main() {
 	//}
 	//log.Printf("cfg.ServerAddress: %s", cfg.ServerAddress)
 	//*ServerAddress := cfg.ServerAddress
+	// 1 вариант
 
 	//os.Setenv("port", ":8080")
 	//os.Setenv("SERVER_ADDRESS", ":8080")
 	//os.Setenv("ServerAddress", "localhost"+os.Getenv("port"))
 	//os.Setenv("BaseURL", "http:/localhost"+os.Getenv("ServerAddress")+"/")
 
-	ServerAddress := flag.String("a", "127.0.0.1:8000", "SERVER_ADDRESS - адрес для запуска HTTP-сервера")
-	flag.Parse()
+	//3 вариант
 	//pflag.StringVarP(*ServerAddress, port)
-	//log.Printf("1 &ServerAddress:")
-	//log.Println(ServerAddress)
+	//3 вариант
+
+	//2 вариант
+	ServerAddress := flag.String(ServerAddress, "127.0.0.1:8000", "SERVER_ADDRESS - адрес для запуска HTTP-сервера")
+	flag.Parse()
 
 	if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
 		*ServerAddress = u
@@ -53,6 +57,7 @@ func main() {
 	flag.Parse()
 	log.Printf("*ServerAddress перед сервером")
 	log.Println(*ServerAddress)
+	//2 вариант
 
 	router := mux.NewRouter()
 
