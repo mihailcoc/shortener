43e0c2ae39d4a9e3bd978a49bd15c60f1148a8f0
charlize theron <mihailcoc@yandex.ru>
2022-04-23 12:13:49 +0300

    My 274 commit

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -5,39 +5,37 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/caarlos0/env"
 	"github.com/gorilla/mux"
-	"github.com/spf13/pflag"
 )
 
 type Config struct {
 	ServerAddress string `env:"SERVER_ADDRESS" envDefault:"127.0.0.1:8080"`
-	BaseURL       string `env:"localhost:8080"`
+	BaseURL       string `env:"http://localhost:8080"`
 }
 
 var (
-	//port          = ":8080"
-	ServerAddress = ":8080"
+	ServerAddress = "127.0.0.1:8080"
 	scheme        = "http"
-	BaseURL       = scheme + "://localhost" + ServerAddress
+	BaseURL       = scheme + ServerAddress
 )
 
 func main() {
 	// 1 вариант
-	//if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
-	//	ServerAddress = u
-	//}
-	//log.Printf("ServerAddress вначале")
-	//log.Println(ServerAddress)
+	log.Printf("ServerAddress вначале:%s", ServerAddress)
+	if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
+		ServerAddress = u
+	}
+	log.Printf("ServerAddress после LookupEnv:%s", ServerAddress)
 
-	//var cfg Config
-	//err := env.Parse(&cfg)
-	//if err != nil {
-	//	log.Fatal(err)
-	//}
-	//log.Printf("cfg.ServerAddress: %s", cfg.ServerAddress)
-	//ServerAddress := cfg.ServerAddress
-	//log.Printf("ServerAddress после env.Parse")
-	//log.Println(ServerAddress)
+	var cfg Config
+	err := env.Parse(&cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("cfg.ServerAddress: %s", cfg.ServerAddress)
+	ServerAddress := cfg.ServerAddress
+	log.Printf("ServerAddress после env.Parse:%s", ServerAddress)
 	// 1 вариант
 
 	//os.Setenv("SERVER_ADDRESS", ":8080")
@@ -62,19 +60,20 @@ func main() {
 	//2 вариант
 
 	//3 вариант
-	os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
-	os.Getenv("SERVER_ADDRESS")
-	if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
-		ServerAddress = u
-	}
-	log.Printf("ServerAddress после LookupEnv")
-	log.Println(ServerAddress)
-	pflag.StringVarP(&ServerAddress, "SERVER_ADDRESS", "s", "127.0.0.1:8000", "SERVER_ADDRESS - адрес для запуска HTTP-сервера")
-	pflag.Parse()
-	log.Printf("ServerAddress после pflag.Parse")
-	log.Println(ServerAddress)
-	log.Printf("ServerAddress перед сервером")
-	log.Println(ServerAddress)
+	//os.Setenv("SERVER_ADDRESS", "127.0.0.1:8080")
+	//os.Getenv("SERVER_ADDRESS")
+
+	//if u, f := os.LookupEnv("SERVER_ADDRESS"); f {
+	//	ServerAddress = u
+	//}
+	//log.Printf("ServerAddress после LookupEnv")
+	//log.Println(ServerAddress)
+	//pflag.StringVarP(&ServerAddress, "SERVER_ADDRESS", "s", "127.0.0.1:8000", "SERVER_ADDRESS - адрес для запуска HTTP-сервера")
+	//pflag.Parse()
+	//log.Printf("ServerAddress после pflag.Parse")
+	//log.Println(ServerAddress)
+	//log.Printf("ServerAddress перед сервером")
+	//log.Println(ServerAddress)
 	//3 вариант
 
 	router := mux.NewRouter()
